e475f13dd7f4efefc65ac13a648960389d98fd43
prorok210 <prorok210c@gmail.com>
2025-02-15 17:55:35 +0300

    Добавил индексы для ускрения запросов, удалил ненужные столбцы из таблиц

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,36 +1,26 @@
 package models
 
-import (
-	"time"
-)
-
-type Model struct {
-	ID        int
-	CreatedAt time.Time
-	UpdatedAt time.Time
-}
-
 type User struct {
-	Model
+	ID       int
 	Name     string `json:"username"`
 	Password string `json:"password,omitempty"`
 	Balance  int    `json:"balance,omitempty"`
 }
 
 type Tokens struct {
-	Model
+	ID          int
 	UserID      int
 	AccessToken string `json:"token"`
 }
 
 type Order struct {
-	Model
+	ID      int
 	UserID  int
 	MerchID int
 }
 
 type Transaction struct {
-	Model
+	ID       int
 	UserID   int
 	ToUserID int
 	ToUser   string `json:"toUser"`
